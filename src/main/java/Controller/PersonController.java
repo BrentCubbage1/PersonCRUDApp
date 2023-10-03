98189961eed340e26699596820cee0b778966b93
Brent <BrentC0328@gmail.com>
2023-10-02 21:36:32 -0400

    post not working

--- src/main/java/Controller/PersonController.java
+++ src/main/java/Controller/PersonController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @Controller
+@RequestMapping(value = "/person")
 public class PersonController {
 
     private PersonService personService;
@@ -28,26 +30,26 @@ public class PersonController {
         return ResponseEntity.status(HttpStatus.OK).body(person);
     }
 
-    @GetMapping(value = "read")
+    @GetMapping(value = "/read")
     public ResponseEntity<List<Person>> readAll(){
         List<Person> personList = personService.readAll();
         return ResponseEntity.status(HttpStatus.OK).body(personList);
     }
 
-    @GetMapping(value = "read/{id}")
+    @GetMapping(value = "/read/{id}")
     public ResponseEntity<Person> readById(@PathVariable Long id){
         Person person = personService.readById(id);
         return ResponseEntity.status(HttpStatus.OK).body(person);
     }
 
-    @PutMapping(value = "update/{id}")
+    @PutMapping(value = "/update/{id}")
     public ResponseEntity<Person> update(@PathVariable Long id, String name){
         Person updatedPerson = personService.update(id, name);
 
         return ResponseEntity.status(HttpStatus.OK).body(updatedPerson);
     }
-    
-    @DeleteMapping(value = "delete/{id}")
+
+    @DeleteMapping(value = "/delete/{id}")
     public ResponseEntity<Person> delete(@PathVariable Long id){
         Person deletedPerson = personService.delete(id);
         return ResponseEntity.status(HttpStatus.OK).body(deletedPerson);
